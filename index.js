9d11188d8365f0a45a1f1295eebdd18fa69ded91
Karolis Bandziulis <G00417529@atu.ie>
2024-12-26 01:32:45 +0000

    Added Home page, Organized Files

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ var ejs = require("ejs")
 var app = express()
 // databases
 var pmysql = require("promise-mysql");
-var mongoDB = require("./mongodb.js")
+var mongoDB = require("./databases/mongodb.js")
+// pages
+var home = require("./pages/home.html")
 // setting render engine
 app.set('view engine', 'ejs')
 // allows access request body
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-
+// application starts and listens on port 3005
 app.listen(3004, () => {
     console.log("Application listening on port 3004");
 })
 
+// home page
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.sendFile(__dirname + "/pages/home.html");
 })
